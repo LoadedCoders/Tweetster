57d4bc56e3bc66bfd838ab5111f915970cf6dfba
pradyumnad <pradyumnadoddala@gmail.com>
2015-05-05 07:28:13 -0500

    Included cli lib. Changes in index.

--- poc/src/com/poc/APIServlet.java
+++ poc/src/com/poc/APIServlet.java
@@ -1,7 +1,11 @@
 package com.poc;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Map;
@@ -11,6 +15,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 
@@ -38,18 +46,59 @@ public class APIServlet extends HttpServlet {
 		String[]qStrings = paramMap.get("q");
 		String command = qStrings[0];
 		
-		JSONObject jsonObject = new JSONObject();
-		if (command.equalsIgnoreCase("count")) {
-			try {
-				String rows = HiveHelper.noOfRows();
-				jsonObject.put("count", rows);
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-//				e.printStackTrace();
-				jsonObject.put("error", e.getLocalizedMessage());
-			}
+		String HADOOP_HOME = "/opt/ibm/biginsights/IHC";
+		Path path = new Path(HADOOP_HOME+"/etc/hadoop/core-site.xml");
+		Path path2 = new Path(HADOOP_HOME+"/etc/hadoop/hdfs-site.xml");
+
+		Configuration configuration = new Configuration();
+		configuration.addResource(path);
+		configuration.addResource(path2);
+
+		FileSystem fs = null;
+		try {
+			fs = FileSystem.get(new URI("hdfs://bivm.ibm.com:9000"), configuration);
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
+		String hdfs_base_uri = "hdfs://bivm.ibm.com:9000/biginsights/hive/warehouse";
+		Path pt = null;
+		
+		if (command.equalsIgnoreCase("count")) {
+			pt = new Path(hdfs_base_uri+"/total_tweets/000000_0");
+		} else if (command.equalsIgnoreCase("langs")) {
+			pt = new Path(hdfs_base_uri+"/total_languages/000000_0");
+		} else if (command.equalsIgnoreCase("users")) {
+			pt = new Path(hdfs_base_uri+"/total_users/000000_0");
+		} else if (command.equalsIgnoreCase("words")) {
+			pt = new Path(hdfs_base_uri+"/total_words/000000_0");
+		} else if (command.equalsIgnoreCase("top_langs")) {
+			pt = new Path(hdfs_base_uri+"/top_languages/000000_0");
+		} else if (command.equalsIgnoreCase("top_pl")) {
+			pt = new Path(hdfs_base_uri+"/top_places/000000_0");
+		} else if (command.equalsIgnoreCase("top_tags")) {
+			pt = new Path(hdfs_base_uri+"/top_hashtags/000000_0");
+		}
+		
+		fs.exists(pt);
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				fs.open(pt)));
+		String line;
+		line = br.readLine();
+		StringBuffer stringBuffer = new StringBuffer();
+		
+		JSONArray jsonObject = new JSONArray();
+
+		while (line != null) {
+			jsonObject.add(line);
+			stringBuffer.append(line);
+			System.out.println(line);
+			line = br.readLine();
+		}
+		
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
 		PrintWriter writer = response.getWriter();
 		writer.println(jsonObject.toJSONString());
 	}
--- poc/src/com/poc/Welcome.java
+++ poc/src/com/poc/Welcome.java
@@ -3,6 +3,9 @@ package com.poc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -39,9 +42,21 @@ public class Welcome extends HttpServlet {
 				"hdfs://bivm.ibm.com:9000/user/biadmin/wc15/count.csv");
 		String HADOOP_HOME = "/opt/ibm/biginsights/IHC";
 		Path path = new Path(HADOOP_HOME+"/etc/hadoop/core-site.xml");
+		Path path2 = new Path(HADOOP_HOME+"/etc/hadoop/hdfs-site.xml");
+
 		Configuration configuration = new Configuration();
-		configuration.getResource(HADOOP_HOME+"/etc/hadoop/core-site.xml");
-		FileSystem fs = FileSystem.get(configuration);
+		configuration.addResource(path);
+		configuration.addResource(path2);
+
+		FileSystem fs = null;
+		try {
+			fs = FileSystem.get(new URI("hdfs://bivm.ibm.com:9000"), configuration);
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		fs.exists(pt);
 		BufferedReader br = new BufferedReader(new InputStreamReader(
 				fs.open(pt)));
 		String line;
